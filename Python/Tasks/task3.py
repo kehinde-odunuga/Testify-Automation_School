1a7cc7ee3633a5be5811e2521f21d8cec7e75a65
kehinde-odunuga <omobolaji92@gmail.com>
2022-10-01 14:52:54 +0100

    initial commit

---
+++ Python/Tasks/task3.py
@@ -0,0 +1,15 @@
+
+# Module 3b Task 3
+
+#Declare a raw string
+str = r'This is a python course\n\tA Testify Automation School initiative!\b'
+
+#Declare two strings with any value
+color = 'White'
+type = ' mansion'
+
+#Concatenate the two string
+house = color + type
+
+#Print out the value of the concatenated strings
+print(house)
