32e1e10f43b97cef74630863d1c12cb034369bdf
odunuga-kehinde <odunugakehinde92@gmail.com>
2023-08-23 20:50:16 +0100

    concatenation

---
+++ module4/Tasks/Task3.java
@@ -0,0 +1,17 @@
+package Tasks;
+
+public class Task3 {
+    public static void main(String[] args) {
+        // Print out age in a string using different methods of concatenation
+
+        int age = 25;
+        String adjective = "My age is ";
+
+        String conjuction = adjective + age;
+
+        System.out.println(conjuction);
+
+        adjective.concat(age+" ");
+        System.out.println(adjective.concat(age+" "));
+    }
+}
