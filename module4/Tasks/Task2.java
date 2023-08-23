0d8743509ec2c8402b4043410cdda1c1165fd4d0
odunuga-kehinde <odunugakehinde92@gmail.com>
2023-08-23 20:31:44 +0100

    Data types

---
+++ module4/Tasks/Task2.java
@@ -0,0 +1,33 @@
+package Tasks;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        // Print out all 6 variables of different data types
+
+        char letter = 'K';
+        System.out.println(letter);
+
+        String car = "Benz";
+        System.out.println(car);
+
+        int count = 75;
+        System.out.println(count);
+
+        boolean isWealthy = true;
+        System.out.println(isWealthy);
+
+        short value = -50;
+        System.out.println(value);
+
+        long salary = 1000000l;
+        System.out.println(salary);
+
+        float temperature = 37.5f;
+        System.out.println(temperature);
+
+        double decimal = 76.67;
+        System.out.println(decimal);
+        
+    }
+}
