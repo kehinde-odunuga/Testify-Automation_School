5855b10fcc28897376d0554e2639ea2fcc3a756f
odunuga-kehinde <odunugakehinde92@gmail.com>
2023-08-23 20:08:23 +0100

    Print output

---
+++ module4/Tasks/Task1.java
@@ -0,0 +1,13 @@
+package Tasks;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Testify!");
+
+        System.out.println("Good day");
+
+        System.out.println("This is my first Java automation task");
+
+        System.out.println("Java is becoming my favorite programming language");
+    }
+}
