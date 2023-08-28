7547988adda8e2b263301b3fc3151552dfcea683
odunuga-kehinde <odunugakehinde92@gmail.com>
2023-08-28 22:49:51 +0100

    String name OOP

---
+++ module4/Tasks/Task11/A.java
@@ -0,0 +1,11 @@
+package Tasks.Task11;
+
+public class A {
+
+    String name;
+
+    public void testName(String newName) {
+        name = newName;
+        System.out.println("My name is " + name);
+    }
+}
---
+++ module4/Tasks/Task11/B.java
@@ -0,0 +1,10 @@
+package Tasks.Task11;
+
+public class B {
+    public static void main(String[] args) {
+        A nameVar = new A();
+        nameVar.name = "Kehinde Odunuga";
+
+        nameVar.testName(nameVar.name);
+    }
+}
