8cc73d7f1647f375eeeefc6ae02b28988e96141c
odunuga-kehinde <odunugakehinde92@gmail.com>
2023-08-30 23:53:49 +0100

    interface

---
+++ module4/Tasks/Task17B/Dashboard.java
@@ -0,0 +1,13 @@
+package Tasks.Task17B;
+
+public class Dashboard implements LoginPage{
+    @Override
+    public void correctUsername() {
+
+    }
+
+    @Override
+    public void correctPassword() {
+
+    }
+}
---
+++ module4/Tasks/Task17B/LoginPage.java
@@ -0,0 +1,8 @@
+package Tasks.Task17B;
+
+public interface LoginPage {
+
+    void correctUsername();
+
+    void  correctPassword();
+}
