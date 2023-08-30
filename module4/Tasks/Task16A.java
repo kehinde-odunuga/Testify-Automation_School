0f2acfe7576f42d57bc605abaffbe9f56da230de
odunuga-kehinde <odunugakehinde92@gmail.com>
2023-08-30 21:20:39 +0100

    overloading

---
+++ module4/Tasks/Task16A.java
@@ -0,0 +1,23 @@
+package Tasks;
+
+public class Task16A {
+
+    public  void methodOverload( int age ){
+
+    }
+    public  void methodOverload( int age, int number ){
+
+    }
+    public  void methodOverload( int age, int number, float price ){
+
+    }
+    public  void methodOverload( boolean availability, int number, float price ){
+
+    }
+    public  void methodOverload( float price, boolean availability, int number ){
+
+    }
+}
+
+
+// OVERLOADING: Create a method in a class and Overload in using the 3 different ways of overloading that you know.
