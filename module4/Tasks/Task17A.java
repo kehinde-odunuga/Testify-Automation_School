7ae1377d8819048e803789239e9c561ed91f8c6e
odunuga-kehinde <odunugakehinde92@gmail.com>
2023-08-30 23:39:53 +0100

    abstraction

---
+++ module4/Tasks/Task17A.java
@@ -0,0 +1,22 @@
+package Tasks;
+
+public abstract class Task17A {
+
+    public abstract void username();
+
+    public abstract void password();
+
+    public abstract void forgotPassword();
+
+    public abstract void signIn();
+
+    public void rememberMe(){
+
+    }
+    public void homePage(){
+
+    }
+    public void OAuth(){
+
+    }
+}
