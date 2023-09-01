297d28090ee7f1babc666ab6f5e617f01a8f1ba0
kehinde-odunuga <omobolaji92@gmail.com>
2023-09-01 23:14:22 +0100

    this keyword

--- module4/Tasks/Task19D.java
+++ module4/Tasks/Task19D.java
@@ -13,8 +13,3 @@ public class Task19D {
         System.out.println(this.name + name);
     }
 }
-
-
- //   THIS https://i.imgur.com/aRkRDPa.png You have a class with a method printName.
-//   write a code in the printName method that will print the instance member which is "Delta"
-//   and whatever name the user enter when invoking the method
