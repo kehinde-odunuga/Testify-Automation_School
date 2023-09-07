521247edf68561f5a9b57344c4dd2c2e9b74f7b9
kehinde-odunuga <omobolaji92@gmail.com>
2023-09-07 20:37:16 +0100

    prime numbers

---
+++ module4/1_Week_Challenge/Challenge4.java
@@ -0,0 +1,31 @@
+package challenge;
+
+public class Challenge4 {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers:");
+
+        for (int num = 2; num <= 100; num++) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int number) {
+
+        if (number <= 3) {
+            return true; // 2 and 3 are prime numbers
+        }
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false; // Numbers divisible by 2 or 3 are not prime
+        }
+
+        for (int i = 5; i * i <= number; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
