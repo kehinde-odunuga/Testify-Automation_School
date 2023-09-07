ba30179bd066d836ecdacd1643bec349131b18c4
kehinde-odunuga <omobolaji92@gmail.com>
2023-09-07 20:14:24 +0100

    initial commit

---
+++ module4/1_Week_Challenge/Challenge1.java
@@ -0,0 +1,40 @@
+package challenge;
+
+public class Challenge1 {
+    public static void main(String[] args) {
+        String number = "10801";
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+
+        String word = "racecar";
+
+        if (isPalindrome(word)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
+
+
+    public static boolean isPalindrome(String str) {
+        // Remove spaces and convert the string to lowercase
+        str = str.replaceAll("\\s", "").toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false; // If characters don't match, it's not a palindrome
+            }
+            left++;
+            right--;
+        }
+
+        return true; // If the loop completes, it's a palindrome
+    }
+}
